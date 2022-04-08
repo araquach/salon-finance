856516a205f4604354218a093c78368864730981
Adam Carter <adam@contra-design.com>
2022-04-08 08:02:27 +0100

    Force ssl

--- cmd/main.go
+++ cmd/main.go
@@ -63,5 +63,18 @@ func main() {
 
 	log.Printf("Starting server on %s", port)
 
-	http.ListenAndServe(":"+port, r)
+	http.ListenAndServe(":"+port, forceSsl(r))
+}
+
+func forceSsl(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if os.Getenv("GO_ENV") == "production" {
+			if r.Header.Get("x-forwarded-proto") != "https" {
+				sslUrl := "https://" + r.Host + r.RequestURI
+				http.Redirect(w, r, sslUrl, http.StatusTemporaryRedirect)
+				return
+			}
+		}
+		next.ServeHTTP(w, r)
+	})
 }
